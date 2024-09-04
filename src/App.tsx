a8e922e932a392ee1671bbb861423c1ca93d34f9
Bryson Oakley <broaken13@gmail.com>
2024-09-03 22:53:08 -0600

    Followed the README's advice. And the changed it.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export default function App() {
       <div className="text-5xl font-title bg-teal-500 text-neutral-200 px-4 py-4">
         Bryson Oakley
       </div>
+      <img className="mt-12 mx-auto" src="./under_construction.png" />
     </>
   );
 }
